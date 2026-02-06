adfb0276d63b848a99eed9e3714a20abf93b27c6
penhsokra <penhsokra@gmail.com>
2026-02-06 16:25:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello CI/CD World!');
 });
 
-// This line "starts" the server
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on http://0.0.0.0:${PORT}`);
-});
-
 module.exports = app; // Export for testing
+
